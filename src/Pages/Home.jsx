f5fec6a70204c8ac9acf8123431e41367d7dda4b
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-10-20 15:32:37 +0530

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,14 +13,14 @@ console.log(data);
 
   return (
     <div>
-        <p>Dashboard</p>
+        <p className='text-[#780000] text-lg font-[700]'>Dashboard</p>
         <select placeholder='Category'>
           <option value="">Men</option>
           <option value="">Women</option>
           <option value="">Jewellery</option>
           <option value="">Electronics</option>
         </select>
-        <div className='grid gap-2 grid-cols-3'>
+        <div className='grid gap-2 grid-cols-3 m-[1rem]'>
           {data.map((e)=>
             <div key={e.id} className='border-2 h-[14rem]'>
               <img src={e.image} alt="" className='w-[80px] ml-[10rem] mb-[1rem]'/>  
